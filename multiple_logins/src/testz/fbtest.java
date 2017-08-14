7e5eadd810d556bbacb82941aa291d86ad46acf9
Gagan <addyrandhaw6@yahoo.com>
2017-08-15 00:38:33 +0530

    #edited

--- multiple_logins/src/testz/fbtest.java
+++ multiple_logins/src/testz/fbtest.java
@@ -16,24 +16,33 @@ public class fbtest extends browserprop {
 	@Test
 	public void loginp() throws Exception {
 		
+	//importing browser from property class
+		
 		prop();
+		
+	// log4j setup
 		
 		Logger log= Logger.getLogger("fbtest");
         PropertyConfigurator.configure("log4j.properties");
         log.info("logger created Sucessfully");
         log.info("browser got opened");
          
-		
+	//importing objects class
+        
         login l=new login(driver);
         log.info("login class imported from objets");
-		
+        
+    //importing excel class
+        
 		exceldrivin ed=new exceldrivin();
 		log.info("exceldrivin class imported from resources");
 		
+	//getiing data from excel
 		ArrayList<String> user=ed.getcelldata(0);
 		ArrayList<String> pass=ed.getcelldata(1);
 		log.info("data received from excel sheet");
 		
+	//Entering data in test case
 		
 		for (int i=0;i<user.size();i++) {
 			
@@ -53,9 +62,10 @@ public class fbtest extends browserprop {
 	}
 	@AfterMethod
 	
-	public void close() {
-		
-		driver.close();
+	// clossing browser
+	
+	        public void close() {
+		    driver.close();
 		
 	}
 
